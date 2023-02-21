421a1b2c75eb37e0864b6ec4ab590fc69ca72291
hu97 <huiung011@gmail.com>
2023-02-21 14:32:29 +0900

    firt commit

--- FirstJava/src/ch1/Ch1.java
+++ FirstJava/src/ch1/Ch1.java
@@ -4,7 +4,8 @@ public class Ch1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("dsfsd");
+		System.out.println("하하하");
+		System.out.println("크크");
 	}
 
 }
